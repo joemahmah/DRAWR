8fb3ea6936a352b941a8ccfc3119cb2d60028468
Michael Hrcek <michael@michaelhrcek.com>
2017-11-02 14:06:42 -0400

    Updated skeleton for generator.

--- generators/basicImageGenerator.go
+++ generators/basicImageGenerator.go
@@ -2,6 +2,7 @@ package generators
 
 import (
 	"image"
+	"image/color"
 	_ "image/png"
 	_ "image/jpeg"
 	"github.com/joemahmah/DRAWR/containers"
@@ -16,4 +17,29 @@ type Parser interface{
 type SimpleGenerator struct {
 	Img			image.Image
 	Storage		containers.SimpleDataManager
+}
+
+/////////////
+//Functions//
+/////////////
+
+func convertPixelto32BitRGBA(pixel containers.Pixel) color.RGBA{
+	return color.RGBA{pixel.R, pixel.G, pixel.B, pixel.A}
+	
+}
+
+///////////
+//Methods//
+///////////
+
+func (generator *SimpleGenerator) SaveImage(path string) error {
+	return nil
+}
+
+func (generator *SimpleGenerator) SetStorage(containers.DataManager) {
+	
+}
+
+func (generator *SimpleGenerator) Generate() error {
+	return nil
 }
