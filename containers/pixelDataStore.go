cebdb44eddd55460927e551f73bff06bfb7a84a9
Michael Hrcek <michael@michaelhrcek.com>
2017-11-10 19:11:07 -0500

    Added tree structure for pixel data storage. Tree should allow for much faster running time. Tree not yet used.

--- containers/pixelDataStore.go
+++ containers/pixelDataStore.go
@@ -8,6 +8,12 @@ import (
 
 var RandomGen *rand.Rand
 
+const (
+	PixelError = 0
+	PixelEqual = 1
+	PixelLess = 2
+	PixelGreater = 3
+)
 
 //Package Init//
 func init(){
@@ -21,6 +27,17 @@ type Pixel struct {
 	A		byte
 	
 	Edge	byte //0 = no, 1 = left, 2 = top
+}
+
+type PixelTreeNode struct {
+	Key			Pixel
+	Count		int
+	LeftNode	*PixelTreeNode
+	RightNode	*PixelTreeNode
+}
+
+type PixelTree struct {
+	RootNode *PixelTreeNode
 }
 
 type PixelData interface{
@@ -61,9 +78,186 @@ func MakeSimpleDataManager() *SimpleDataManager{
 	return sdm
 }
 
+func MakePixelTreeNode(pixel Pixel, count int) *PixelTreeNode{
+	newNode := new(PixelTreeNode)
+	newNode.Key = pixel
+	newNode.Count = count
+	newNode.LeftNode = nil
+	newNode.RightNode = nil
+	
+	return newNode
+}
+
+
+//Note: 1=equal, 2=lessthan, 3=greaterthan, 0=error
+//Equality based on R then G then B then A then Edge
+func GetPixelRelation(pixel1 *Pixel, pixel2 *Pixel) int{
+
+	if(pixel1.R > pixel2.R){
+		return PixelGreater
+		
+	} else if (pixel1.R < pixel2.R) {
+		return PixelLess
+		
+	} else { //If reds are the same
+		if(pixel1.G > pixel2.G){
+			return PixelGreater
+			
+		} else if (pixel1.G < pixel2.G) {
+			return PixelLess
+			
+		} else { //If greens are the same
+			if(pixel1.B > pixel2.B){
+				return PixelGreater
+				
+			} else if (pixel1.B < pixel2.B) {
+				return PixelLess
+				
+			} else { //If blues are the same
+					if(pixel1.A > pixel2.A){
+						return PixelGreater
+						
+					} else if (pixel1.A < pixel2.A) {
+						return PixelLess
+						
+					} else { //If alphas are the same
+						if(pixel1.Edge > pixel2.Edge){
+							return PixelGreater
+							
+						} else if (pixel1.Edge < pixel2.Edge) {
+							return PixelLess
+							
+						} else { //If edge states are the same
+							//These are the same
+							return PixelEqual
+						}
+					}
+			}
+		}
+	}
+	
+	//This should never happen
+	//But if it does...
+	return PixelError
+}
+
 ///////////
 //Methods//
 ///////////
+
+// PixelTreeNode
+func (node *PixelTreeNode) Add(pixel Pixel, count int){
+	relation := GetPixelRelation(&node.Key, &pixel)
+	
+	switch relation {
+	case PixelEqual:
+		node.Count += count;
+		
+	case PixelGreater:
+		if(node.RightNode == nil){ //If node empty
+			node.RightNode = MakePixelTreeNode(pixel, count)
+		} else { //If node full
+			node.RightNode.Add(pixel, count)
+		}
+	
+	case PixelLess:
+		if(node.LeftNode == nil){ //If node empty
+			node.LeftNode = MakePixelTreeNode(pixel, count)
+		} else { //If node full
+			node.LeftNode.Add(pixel, count)
+		}
+
+	default:
+		//TODO: Add error handling
+	}
+}
+
+//returns the node with key pixel (if not exist, nil)
+func (node *PixelTreeNode) GetNode(pixel Pixel) *PixelTreeNode{
+	relation := GetPixelRelation(&node.Key, &pixel)
+	
+	switch relation {
+	case PixelEqual:
+		return node
+		
+	case PixelGreater:
+		if(node.RightNode == nil){ //If node empty
+			return nil
+		} else { //If node full
+			return node.RightNode.GetNode(pixel)
+		}
+	
+	case PixelLess:
+		if(node.LeftNode == nil){ //If node empty
+			return nil
+		} else { //If node full
+			return node.LeftNode.GetNode(pixel)
+		}
+
+	default:
+		return nil
+	}
+	
+	//Should never hit here
+	return nil
+}
+
+//returns the node with key pixel (if not exist, nil)
+func (node *PixelTreeNode) GetCount(pixel Pixel) (int, error){
+	relation := GetPixelRelation(&node.Key, &pixel)
+	
+	switch relation {
+	case PixelEqual:
+		return node.Count, nil
+		
+	case PixelGreater:
+		if(node.RightNode == nil){ //If node empty
+			return 0, errors.New("Node does not exist.")
+		} else { //If node full
+			return node.RightNode.GetCount(pixel)
+		}
+	
+	case PixelLess:
+		if(node.LeftNode == nil){ //If node empty
+			return 0, errors.New("Node does not exist.")
+		} else { //If node full
+			return node.LeftNode.GetCount(pixel)
+		}
+
+	default:
+		return 0, errors.New("Node does not exist.")
+	}
+	
+	//Should never hit here
+	return 0, errors.New("Node does not exist.")
+}
+
+//PixelTree
+func (tree *PixelTree) Add(pixel Pixel, count int){
+	if(tree.RootNode != nil) {
+		tree.RootNode.Add(pixel, count)
+	} else { //if tree empty
+		tree.RootNode = MakePixelTreeNode(pixel, count)
+	}
+
+}
+
+func (tree *PixelTree) GetNode(pixel Pixel) *PixelTreeNode{
+	if(tree.RootNode != nil) {
+		return tree.RootNode.GetNode(pixel);
+	} else { //if tree empty
+		return nil
+	}
+
+}
+
+func (tree *PixelTree) GetCount(pixel Pixel) (int, error){
+	if(tree.RootNode != nil) {
+		return tree.RootNode.GetCount(pixel);
+	} else { //if tree empty
+		return 0, errors.New("Tree empty.")
+	}
+}
 
 //Pixel
 func (p *Pixel) SetRGB(r,g,b byte){
